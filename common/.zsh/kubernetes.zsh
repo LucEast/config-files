5c281383aa4e127f8f23078276398dcdd900f991
LucEast <47122750+LucEast@users.noreply.github.com>
2024-06-21 09:26:39 +0200

    Update kubernetes.zsh

--- common/.zsh/kubernetes.zsh
+++ common/.zsh/kubernetes.zsh
@@ -1,5 +1,14 @@
+# Alias für kubectl
 alias k=kubectl
-complete -F __start_kubectl k
 
-alias kx='f() { [ \"\$1\" ] && kubectl config use-context \$1 || kubectl config current-context ; } ; f'
-alias kn='f() { [ \"\$1\" ] && kubectl config set-context --current --namespace \$1 || kubectl config view --minify | grep namespace | cut -d\" \" -f6 ; } ; f'
+# Autokomplettierung für kubectl
+autoload -U +X compinit && compinit
+autoload -U +X bashcompinit && bashcompinit
+source <(kubectl completion zsh)
+compdef k=kubectl
+
+# Alias für das Wechseln des Kontexts
+alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config current-context ; } ; f'
+
+# Alias für das Setzen des Namespaces
+alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f2 ; } ; f'
